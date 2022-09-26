a9dc608455acf0c0179633d03db1fd56049b1ee5
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-26 10:13:50 +0530

    Create operation done

--- routes.js
+++ routes.js
@@ -1,15 +1,28 @@
-const { app } = require('./server');
-const list = require('./models/list');
+const { app, express } = require('./server');
+const read = require('./CRUD/read');
+const create = require('./CRUD/create');
+
+app.use(express.json());
 
 app.get('/', async (req, resp) => {
     resp.setHeader('Content-Type', 'application/json');
     const { query } = req;
     try {
-        const data = await list(query);
-        resp.send(data);
+        const data = await read(query);
+        resp.status(200).send(data);
     } catch(error) {
-        resp.send({ error })
+        resp.status(400).send({ error })
     }
+});
+
+app.post('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/text');
+    create(req.body)
+    .then((resp) => {
+        const { acknowledged, insertedId } = resp;
+        if(acknowledged) res.status(200).send(`Data saved with id: ${insertedId}`);
+        else res.status(403).send("Cannot save data")
+    }).catch((error) => res.status(400).send(error))
 });
 
 module.exports = app;
