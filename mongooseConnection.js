8c949d760aba6019028231a1e094f33bb4824318
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-27 13:43:42 +0530

    Add mongooseConnection file

---
+++ mongooseConnection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const URL = 'mongodb://localhost:27017';
+
+const DB = 'e-comm';
+
+const connection = async () => {
+    return mongoose.connect(`${URL}/${DB}`);
+}
+
+module.exports = { connection, mongoose };
